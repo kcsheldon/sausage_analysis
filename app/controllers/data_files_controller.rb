438a3b04ada36123e4a0b82bad13a8b70ce7e820
Kathleen Sheldon <kcsheldon@verizon.net>
2015-07-21 15:42:11 -0400

    add code to chart sausage data in c3

--- app/controllers/data_files_controller.rb
+++ app/controllers/data_files_controller.rb
@@ -27,17 +27,53 @@ class DataFilesController < ApplicationController
     #load the selected CSV file from S3 into the database
     @data_file = DataFile.find(params[:id])
     @url = @data_file.csv_file.url
-    #binding.pry
     @file = CSV.parse(open(@url))
-    # gon.data = @file.to_json
-    check_url = @data_file.csv_file.url
 
-    respond_to do |format|
-      format.html
-      format.json {
-        render :json => @file.to_json
-      }
+    @file[0][0] = 'x'
+
+    @x_time = []
+    @data1 = []
+    @data2 = []
+    @data3 = []
+    @data4 = []
+    @data5 = []
+    @data6 = []
+    @data7 = []
+    @data8 = []
+    @data9 = []
+
+    @file.each do |array|
+      @x_time << array[0]
+      @data1 << array[1]
+      @data2 << array[2]
+      @data3 << array[3]
+      @data4 << array[4]
+      @data5 << array[5]
+      @data6 << array[6]
+      @data7 << array[7]
+      @data8 << array[8]
     end
+
+    # gon.data = @file.to_json
+    # check_url = @data_file.csv_file.url
+
+    # Code from Christina
+    # respond_to do |format|
+    #   format.html
+    #   format.json {
+    #     render :json => @file.to_json
+    #   }
+    # end
+    gon.x_time = @x_time
+    gon.data1 = @data1
+    gon.data2 = @data2
+    gon.data3 = @data3
+    gon.data4 = @data4
+    gon.data5 = @data5
+    gon.data6 = @data6
+    gon.data7 = @data7
+    gon.data8 = @data8
+
   end
 
   protected
